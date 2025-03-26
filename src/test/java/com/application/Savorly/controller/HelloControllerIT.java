995ea0d867109b252c4fe6ffa8b3f080d83b2be8
Piyush <piyushmulchandani03@gmail.com>
2025-03-26 20:37:03 +0100

    Modified coverage report configuration

--- src/test/java/com/application/Savorly/controller/HelloControllerIT.java
+++ src/test/java/com/application/Savorly/controller/HelloControllerIT.java
@@ -6,16 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,20 +35,5 @@ class HelloControllerIT {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/hello")
                         .queryParam("name", "Name"))
                 .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    @WithMockCustomUser(username = "adminUser", role = "admin")
-    void shouldReturnAuthenticatedUser() throws Exception {
-        UserDetails userDetails = new User("adminUser", "password",
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_client_admin")));
-        SecurityContext context = SecurityContextHolder.createEmptyContext();
-        context.setAuthentication(new org.springframework.security.authentication.UsernamePasswordAuthenticationToken(
-                userDetails, "password", userDetails.getAuthorities()));
-        SecurityContextHolder.setContext(context);
-
-        MvcResult actual = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/users/me"))
-                .andExpect(status().isOk())
-                .andReturn();
     }
 }
